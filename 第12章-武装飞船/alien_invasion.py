c0cb8de4859eedab26959e701184385f10252c06
Dabuliule <614151250@qq.com>
2022-12-05 09:17:24 +0800

    2022/12/05

---
+++ 第12章-武装飞船/alien_invasion.py
@@ -0,0 +1,33 @@
+import sys
+import pygame
+from settings import Settings
+
+
+def run_game():
+    # 初始化背景设置
+    pygame.init()
+    ai_settings = Settings()
+
+    # 调用 pygame.display.set_mode() 来创建一个名为 screen 的显示窗口
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height)
+    )
+
+    pygame.display.set_caption("Alien Invasion")
+
+    # 开始游戏的主循环
+    while True:
+
+        # 监视键盘和鼠标事件
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        # 每次循环都重新绘制屏幕
+        screen.fill(ai_settings.bg_color)
+
+        # 让最近绘制的屏幕可见
+        pygame.display.flip()
+
+
+run_game()
