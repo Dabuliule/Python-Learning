e39ba0f70ba60c65efeb351dcef39c2d4a12363e
Dabuliule <614151250@qq.com>
2022-12-08 23:32:30 +0800

    2022/12/08

--- 第12章-武装飞船/alien_invasion.py
+++ 第12章-武装飞船/alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 
 
 def run_game():
@@ -23,9 +24,7 @@ def run_game():
     while True:
 
         # 监视键盘和鼠标事件
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+        gf.check_events()
 
         # 每次循环都重新绘制屏幕
         screen.fill(ai_settings.bg_color)
